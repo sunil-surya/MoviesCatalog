769f150e5f3818d875f83a036a9c95206fa35b0e
Sunil <tsunil8112@gmail.com>
2020-07-26 22:39:01 +0530

    Added 3 microservices and initial environment setup

---
+++ movie-rating-service/src/main/java/in/sunil/movierateservice/MovieRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package in.sunil.movierateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingServiceApplication.class, args);
+	}
+
+}
---
+++ movie-rating-service/src/test/java/in/sunil/movierateservice/MovieRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sunil.movierateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
